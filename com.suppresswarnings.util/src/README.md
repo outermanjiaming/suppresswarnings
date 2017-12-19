f7c6d844393b0881ebc9fcef1e8da9a5f3103fa9
lijiaming <lijiaming@meizu.com>
2017-12-19 19:49:33 +0800

    start to construct

---
+++ com.suppresswarnings.util/src/README.md
@@ -0,0 +1,2 @@
+### Usage ###
+** java -cp com.suppresswarnings.util.jar com.suppresswarnings.util.Keyboard 1 28 15 **
--- com.suppresswarnings.util/src/com/suppresswarnings/util/Keyboard.java
+++ com.suppresswarnings.util/src/com/suppresswarnings/util/Keyboard.java
@@ -1,48 +1,93 @@
 package com.suppresswarnings.util;
 
+import java.awt.BorderLayout;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Date;
+import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class Keyboard {
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class Keyboard extends JFrame implements MouseListener {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6967961653164581153L;
 	Robot robot;
 	long clicked = 0;
 	boolean closed = true;
 	ScheduledExecutorService service;
-	public Keyboard() {
+	private JTextArea statusBar;
+	public Keyboard(String title) {
+		 super(title);   
+         statusBar = new JTextArea();   
+         statusBar.setAutoscrolls(true);
+         JScrollPane scrollPane = new JScrollPane();
+         scrollPane.setViewportView(statusBar);
+         scrollPane.setAutoscrolls(true);
+         scrollPane.setFocusable(true);
+         getContentPane().add(scrollPane, BorderLayout.CENTER);
+         statusBar.addMouseListener(this);   
+         setSize(465,100);
+         setLocation(-695, 800);
+         setVisible(true);
 		try {
 			robot = new Robot();
-			robot.setAutoDelay(20);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
-	public void autoClick(int key, long start, long period, TimeUnit unit, long gap){
+	public void autoCtrlShiftAltKeyClick(int keyEventKey, long start, long period, TimeUnit unit, long gap){
 		Runnable command = new Runnable() {
 			long lastTime = System.currentTimeMillis();
 			@Override
 			public void run() {
-				robot.keyPress(key);
-				robot.keyRelease(key);
+				robot.keyPress(KeyEvent.VK_CONTROL);
+				robot.keyPress(KeyEvent.VK_SHIFT);
+				robot.keyPress(KeyEvent.VK_ALT);
+				robot.keyPress(keyEventKey);
+				robot.delay(20);
+				robot.keyRelease(KeyEvent.VK_CONTROL);
+				robot.keyRelease(KeyEvent.VK_SHIFT);
+				robot.keyRelease(KeyEvent.VK_ALT);
+				robot.keyRelease(keyEventKey);
 				clicked ++;
 				closed = !closed;
 				long now = System.currentTimeMillis();
 				long time = now - lastTime;
-				System.out.println("( " + TimeUnit.MILLISECONDS.toSeconds(time) + "s ) " + new Date(now) + " [" + key + "] key clicked " + clicked + " times, " + (closed ? "closed" : "opened"));
+				println("( " + TimeUnit.MILLISECONDS.toSeconds(time) + "s ) " + new Date(now) + " [" + keyEventKey + "] key clicked " + clicked + " times, " + (closed ? "closed" : "opened"));
 				lastTime = now;
 				if(closed) {
 					try {
+						robot.mouseMove(-666, 444);
+						robot.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
+						robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
 						unit.sleep(gap);
+						robot.mouseMove(-666, 444);
+						robot.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
+						robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
-					robot.keyPress(key);
-					robot.keyRelease(key);
+					robot.keyPress(KeyEvent.VK_CONTROL);
+					robot.keyPress(KeyEvent.VK_SHIFT);
+					robot.keyPress(KeyEvent.VK_ALT);
+					robot.keyPress(keyEventKey);
+					robot.delay(20);
+					robot.keyRelease(KeyEvent.VK_CONTROL);
+					robot.keyRelease(KeyEvent.VK_SHIFT);
+					robot.keyRelease(KeyEvent.VK_ALT);
+					robot.keyRelease(keyEventKey);
 					closed = !closed;
-					System.out.println("( " + gap + "ms ) " + new Date() + " [" + key + "] key clicked " + clicked + " times, " + (closed ? "closed" : "opened"));
+					println("( " + TimeUnit.MILLISECONDS.toSeconds(gap) + "s ) " + new Date() + " clicked " + clicked + " times, " + (closed ? "closed" : "opened"));
 				}
 			}
 		};
@@ -52,7 +97,46 @@ public class Keyboard {
 	}
 	
 	public static void main(String[] args) {
-		Keyboard kb = new Keyboard();
-		kb.autoClick(KeyEvent.VK_F12, TimeUnit.SECONDS.toMillis(1), TimeUnit.MINUTES.toMillis(28), TimeUnit.MILLISECONDS, TimeUnit.MILLISECONDS.toMillis(666));
+		System.out.println("It'll click [Ctrl + Shift + Alt + Win] automatically with fixed delay.");
+		System.out.println("[#start]s [#delay]m [#break]s");
+		if(args.length < 3) {
+			System.out.println("Wrong args...");
+			return;
+		}
+		long start = Long.parseLong(args[0]);
+		long delay = Long.parseLong(args[1]);
+		long broke = Long.parseLong(args[2]);
+		String title = String.format("start in %ss, auto-click every %sm, rest %ss each time", start, delay, broke);
+		System.out.println(title);
+		System.out.println("Continue? y/N");
+		Scanner scan = new Scanner(System.in);
+		String yes = scan.nextLine();
+		scan.close();
+		if(!"y".equals(yes)) {
+			return;
+		}
+		Keyboard kb = new Keyboard(title);
+		kb.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		kb.autoCtrlShiftAltKeyClick(KeyEvent.VK_WINDOWS, TimeUnit.SECONDS.toMillis(start), TimeUnit.MINUTES.toMillis(delay), TimeUnit.MILLISECONDS, TimeUnit.SECONDS.toMillis(broke));
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		println("mouseClicked " + e.getLocationOnScreen());
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+	
+	public void println(String line) {
+		statusBar.append(line+"\n");
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
 	}
 }
