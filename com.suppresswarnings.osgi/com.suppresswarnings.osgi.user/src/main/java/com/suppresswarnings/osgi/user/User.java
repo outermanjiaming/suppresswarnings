be4f5a3359d3d595a45835322f53814fc6831aa5
lijiaming <outermanjiaming@163.com>
2017-12-24 19:39:51 +0800

    last login

--- com.suppresswarnings.osgi/com.suppresswarnings.osgi.user/src/main/java/com/suppresswarnings/osgi/user/User.java
+++ com.suppresswarnings.osgi/com.suppresswarnings.osgi.user/src/main/java/com/suppresswarnings/osgi/user/User.java
@@ -10,7 +10,7 @@ import java.util.Random;
 public class User {
 	public static final String HEAD = "00000";
 	public static enum KEY {
-		UID, Name, Invite, Invited, InviteCount, Account, AccountCount;
+		UID, Name, Invite, Invited, InviteCount, Account, AccountCount, LastLogin;
 	}
 	String uid;
 	Map<String, String> attributs;
