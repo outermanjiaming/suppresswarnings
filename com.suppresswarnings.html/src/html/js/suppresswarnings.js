152bffe52fdcafaa6bc768fc09a461f77b024504
lijiaming <outermanjiaming@163.com>
2018-01-23 08:32:24 +0800

    nn

--- com.suppresswarnings.html/src/html/js/suppresswarnings.js
+++ com.suppresswarnings.html/src/html/js/suppresswarnings.js
@@ -1,36 +1,33 @@
 var index = 0;
 var current;
-var cmd = function() {
-	var r = Math.random();
-	$.ajax("index.html")
-	.done(function(data, textStatus, jqXHR) {
-		alert("success " + data);
-	})
-	.fail(function() {
-		console.log("error" + r);
-	})
-	.always(function() {
-		var cmdtxt = $('#cmdinput').val();
-		if(current == cmdtxt) {
-			$('#cmdinput').val('');
-		}
-		$("#msg").append( "<li style='list-style-type:none;'> "+ index +" <a href='"+server.protocol + server.host+"'> Successful Request!</a>" + window.token + "</li>");
-		$("#msg").scrollTop($("#msg")[0].scrollHeight);
-		server.action('user.http?action=Login');
-		$("#panelLogin").slideDown();
-		index ++;
-	});
-
-}
 const userLogin = "user.http?action=Login";
 const userRegsiter = "user.http?action=Regsiter";
 const userInvite = "user.http?action=Invite";
 const userInvited = "user.http?action=Invited";
-const interaction = "interaction.http?action=Input"
+const interaction = "user.http?action=Interaction"
 var server = {
 	protocol : 'http://',
 	host : '139.199.104.224/',
 	uri : 'interaction.http',
+	ajax : function(obj, args) {
+		var r = Math.random();
+		$.ajax(this.protocol + this.host + this.uri + "&" + args)
+		.done(function(data, textStatus, jqXHR) {
+			$("#msg").append( "<li style='list-style-type:none;'><a href='"+server.protocol + server.host+"'> Always </a>" + data + "</li>");
+			$("#msg").scrollTop($("#msg")[0].scrollHeight);
+		})
+		.fail(function() {
+			console.log("error" + r);
+		})
+		.always(function() {
+			var cmdtxt = $('#cmdinput').val();
+			if(current == cmdtxt) {
+				$('#cmdinput').val('');
+			}
+			$("#msg").append( "<li style='list-style-type:none;'><a href='"+server.protocol + server.host+"'> Always </a>" + window.token + "</li>");
+			$("#msg").scrollTop($("#msg")[0].scrollHeight);
+		});
+	},
 	alert : function() {
 		alert('server alert ' + this.protocol + this.host + this.uri)
 	},
