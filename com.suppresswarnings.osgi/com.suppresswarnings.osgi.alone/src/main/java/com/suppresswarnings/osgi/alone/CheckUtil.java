4bb719e78649870f482528e2a493f68be3906f31
lijiaming <lijiaming@meizu.com>
2018-03-02 16:41:23 +0800

    clean str

--- com.suppresswarnings.osgi/com.suppresswarnings.osgi.alone/src/main/java/com/suppresswarnings/osgi/alone/CheckUtil.java
+++ com.suppresswarnings.osgi/com.suppresswarnings.osgi.alone/src/main/java/com/suppresswarnings/osgi/alone/CheckUtil.java
@@ -1,9 +1,23 @@
 package com.suppresswarnings.osgi.alone;
 
+import java.util.regex.Pattern;
+
 public class CheckUtil {
+	public static final String symbolRegex = "[`~!@#$%^&*()+=|{}':;'\",\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+	public static final Pattern symbolPattern = Pattern.compile(symbolRegex);
 	public static boolean anyNull(String...others) {
 		if(others == null) return true;
 		for(String s : others) if(s == null) return true;
 		return false;
+	}
+	
+	public static String cleanStr(String str) {
+		if(str == null) return null;
+		return symbolPattern.matcher(str.trim()).replaceAll("");
+	}
+	
+	public static void main(String[] args) {
+		String clean = cleanStr("!@我要答题。。%");
+		System.out.println(clean);
 	}
 }
