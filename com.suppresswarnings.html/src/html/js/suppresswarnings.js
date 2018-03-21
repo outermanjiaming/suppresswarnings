b088c94f4ae2dbba1a739fa6f4fb506bddd7a8ef
lijiaming <lijiaming@meizu.com>
2018-03-21 17:19:53 +0800

    logo new

--- com.suppresswarnings.html/src/html/js/suppresswarnings.js
+++ com.suppresswarnings.html/src/html/js/suppresswarnings.js
@@ -1,7 +1,22 @@
-$(document).ready(function(){
-	$("#root").html("Welcome Home")
-
-　　$("#root").click(function(){ 
-　　　　 $("#root").html("<a href='http://suppresswarnings.com/index.html'>素朴网联</a>")
-　　}); 
-}); 
+  var ids = ["ul","u","ur","mr","dr","d","dl","ml"]
+  var index = 0
+  var forever = 10000
+  function red(){
+    $("#"+"svg_"+ids[index%8]).css("stroke","pink");
+    if(index<forever) {
+      setTimeout("green()", 100);
+    } else {
+      $("#" + "svg_"+ids[index%8]).css("stroke","#5cb85c");
+    }
+  }
+  function green(){
+    $("#" + "svg_"+ids[index%8]).css("stroke","#5cb85c");
+    if(index<forever) {
+      setTimeout("red()", 100);
+    }
+    index++;
+  }
+  
+  $(document).ready(function(){
+    green();
+  }); 
