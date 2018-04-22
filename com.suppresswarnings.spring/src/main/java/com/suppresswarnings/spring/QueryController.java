f3e9871cd56293bd47408f6e6a05b0dc8c4c0b87
lijiaming <outermanjiaming@163.com>
2018-04-22 12:20:38 +0800

    from mac

---
+++ com.suppresswarnings.spring/src/main/java/com/suppresswarnings/spring/QueryController.java
@@ -0,0 +1,13 @@
+package com.suppresswarnings.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QueryController {
+
+	@RequestMapping("/hello")
+    public String index() {
+        return "Hello from Spring Boot!";
+    }
+}
