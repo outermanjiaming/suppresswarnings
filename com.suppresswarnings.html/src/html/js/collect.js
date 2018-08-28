de3abcf3bab202770d7bd76afb58b952fed89e18
lijiaming <outermanjiaming@163.com>
2018-08-28 11:13:37 +0800

    collect js replyquiz

--- com.suppresswarnings.html/src/html/js/collect.js
+++ com.suppresswarnings.html/src/html/js/collect.js
@@ -1,3 +1,30 @@
+
+function replyquiz(obj) {
+	var quizId = $(obj).data("quizId")
+	var reply = $("#replyquiz").val()
+	
+	jQuery.ajax({
+    url: "/wx.http?r=" + Math.random(),
+    data: {
+	    action : "replyquiz",
+	    random : randnum,
+	    ticket : ticket,
+	    state : state,
+	    reply : reply
+    },
+    success: function( result ) {
+    	if("fail" == result) {
+    		$("#replyquiz").val("提交失败，稍后重试")
+    	} else {
+    		$("#replyquiz").val("提交成功，可以继续提交")
+    	}
+    	
+    },
+    error: function( xhr, result, obj ) {
+    	$("#replyquiz").val("服务端错误，请按要求输入重试")
+    }
+  })
+}
 jQuery.ajax({
     url: "/wx.http?r=" + Math.random(),
     data: {
@@ -21,6 +48,11 @@ jQuery.ajax({
         	$("#crewimg").append("<img style='width: 20px;height: 20px;margin-right: 5px;margin-top:2px;' src='" + arr[k] + "'/>");
         }
         
+        var myimg = collect.userimg
+        var myname = collect.username
+        $("#business").append("<div class='form-group text-center'><input id='replyquiz' type='text' class='input btn-xs' placeholder='请按要求输入回复内容' size='35'><button type='button' class='btn btn-xs' data-quizId='"+state+"' onclick='replyquiz(this)'>回复</button></div>")
+        $("#business").append("<input id='userimg' type='text' class='sr-only hidden' value='"+collect.userimg+"'/>")
+        $("#business").append("<input id='username' type='text' class='sr-only hidden' value='"+collect.username+"'/>")
       }
     },
     error: function( xhr, result, obj ) {
@@ -28,18 +60,4 @@ jQuery.ajax({
       $('#inviteTitle').text('!素朴网联')
       oauth2()
     }
-  })
-function hideme(obj) {
-	$(obj).hide()
-}
-function addon(){
-	 $("#business").append("<li class='boder_v1 addon' onclick='hideme(this)'><span>addon</span></li>")
-}
-$(document).ready(function(){
-  $(".replyinput").click(function(){
-    $("p").slideToggle();
-  });
-  $(".boder_v1").click(function(){
-	  $("#business").append("<li class='boder_v1' onclick='addon()'><span>test</span></li>")
-  })
-});
+})
