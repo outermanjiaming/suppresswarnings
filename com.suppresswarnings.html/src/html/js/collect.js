c5137ca322dddce163ea525f37aa839997db0622
lijiaming <outermanjiaming@163.com>
2018-08-28 12:47:12 +0800

    collect js snedreply

--- com.suppresswarnings.html/src/html/js/collect.js
+++ com.suppresswarnings.html/src/html/js/collect.js
@@ -1,14 +1,30 @@
 function newreply(myimg, reply, replyId) {
-	var li = "<li class='boder_v1'><img style='width: 20px;height: 20px;margin-right: 5px;margin-top:2px;' src='"+myimg+"'><span>"+reply+"</span><div class='form-group text-right'><button type='button' class='btn btn-xs'>同义句</button>   <button type='button' class='btn btn-xs'>回复</button></div><div class='replyinput form-group text-center'><input type='text' class='input btn-xs' placeholder='请输入回复内容' size='33'><button type='button' data-replyId='"+replyId+"' data-which='replysame' class='btn btn-xs' onclick='replyinput(this)'>发送</button></div></li>"
+	var li = "<li class='boder_v1'><img style='width: 20px;height: 20px;margin-right: 5px;margin-top:2px;' src='"+myimg+"'><span>"+reply+"</span><div class='form-group text-center'><button type='button' class='btn btn-xs' onclick='changesame(this)'>同义句</button> - <button type='button' class='btn btn-xs' onclick='changereply(this)'>回复</button><div class='sendreplydiv hidden' style='padding-top:15px;'><input type='text' class='input btn-xs sendreplyinput' placeholder='请输入回复内容' size='30'><button type='button' data-replyid='"+replyId+"' data-which='replysame' class='btn btn-xs sendreplybtn' onclick='sendreply(this)'>发送</button></div></div></li>"
 	$("#business").append(li)
 }
-function replyinput(obj) {
-	var replyId = $(obj).data("replyId")
+function changesame(obj) {
+	var sendreplydiv = $(obj).siblings(".sendreplydiv")[0]
+	$(sendreplydiv).show()
+	var sendreplybtn = $(sendreplydiv).children(".sendreplybtn")[0]
+	$(sendreplybtn).data("which", "same")
+}
+function changereply(obj) {
+	var sendreplydiv = $(obj).siblings(".sendreplydiv")[0]
+	$(sendreplydiv).show()
+	var sendreplybtn = $(sendreplydiv).children(".sendreplybtn")[0]
+	$(sendreplybtn).data("which", "reply")
+}
+function sendreply(obj) {
+	var replyId = $(obj).data("replyid")
 	var which = $(obj).data("which")
 	console.log(replyId + " & " + which)
+	var sendreplyinput = $(obj).siblings(".sendreplyinput")[0]
+	var reply = $(sendreplyinput).val()
+	alert(reply)
+	$(sendreplyinput).val("")
 }
 function replyquiz(obj) {
-	var quizId = $(obj).data("quizId")
+	var quizId = $(obj).data("quizid")
 	var reply = $("#replyquiz").val()
 	var myimg = $("#userimg").val()
 	var myname= $("#username").val()
@@ -28,6 +44,7 @@ function replyquiz(obj) {
     		$("#replyquiz").attr("placeholder","提交成功，可以继续提交")
     		newreply(myimg, reply, state)
     	}
+    	$("#replyquiz").val("")
     	$("#replyquiz").focus()
     },
     error: function( xhr, result, obj ) {
@@ -60,7 +77,7 @@ jQuery.ajax({
         
         var myimg = collect.userimg
         var myname = collect.username
-        $("#business > .boder_v1").append("<div class='form-group text-center'><input id='replyquiz' type='text' class='input btn-xs' placeholder='请按要求输入回复内容' size='33'><button type='button' class='btn btn-xs' data-quizId='"+state+"' onclick='replyquiz(this)'>回复</button></div>")
+        $("#business > .boder_v1").append("<div class='form-group text-center'><input id='replyquiz' type='text' class='input btn-xs' placeholder='请按要求输入回复内容' size='33'><button type='button' class='btn btn-xs' data-quizid='"+state+"' onclick='replyquiz(this)'>回复</button></div>")
         $("#business").append("<input id='userimg' type='text' class='sr-only hidden' value='"+collect.userimg+"'/>")
         $("#business").append("<input id='username' type='text' class='sr-only hidden' value='"+collect.username+"'/>")
       }
