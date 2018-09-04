d26600ed1c8d34c4fc8ba2482d24a0819014c466
lijiaming <outermanjiaming@163.com>
2018-09-04 09:54:12 +0800

    daigou addgoodstocart

--- com.suppresswarnings.html/src/html/js/daigou.js
+++ com.suppresswarnings.html/src/html/js/daigou.js
@@ -1,4 +1,28 @@
-
+function addtocart(obj) {
+	var goodsid = $(obj).data("goodsid")
+	var agentid = $(obj).data("agentid")
+	jQuery.ajax({
+	    url: "/wx.http?r=" + Math.random(),
+	    data: {
+		    action : "daigou",
+		    todo : "addgoodstocart",
+		    random : randnum,
+		    ticket : ticket,
+		    goodsid: goodsid,
+		    state : agentid
+	    },
+	    success: function( result ) {
+	      if("fail" == result) {
+	    	  console.log('fail to addgoodstocart: ' + result)
+	      } else {
+	    	  console.log('great addgoodstocart')
+	      }
+	    },
+	    error: function( xhr, result, obj ) {
+	      console.log("[lijiaming] addgoodstocart err: " + result)
+	    }
+	})
+}
 function addone(goods) {
 	var rate = 0.01
 	var price = rate * parseFloat(goods.pricecent)
@@ -13,7 +37,7 @@ function addone(goods) {
           '<dt><a href="/detail.html?agentid=who&code=what&goodsid=which">'+goods.title+'</a></dt>' +
 	  	  '<dd style="font-size:12px;">'+goods.extra+'</dd>' + 
 	      '<dd><i>￥'+price+'</i></dd>' + 
-	      '<dd><a class="rbtn mini-addcart" href="javascript:;" data-agentid="'+state+'" data-goodsid="' +goods.goodsid+ '"> 加入购物车 </a></dd>' +
+	      '<dd><a class="rbtn mini-addcart" href="javascript:;" onclick="addtocart(this)" data-agentid="'+state+'" data-goodsid="' +goods.goodsid+ '"> 加入购物车 </a></dd>' +
     '</dl>' +
   '</div>' +
 '</li>'
