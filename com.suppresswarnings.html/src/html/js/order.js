e2a7e7b44220df875dfbd75daadc4f63fd5f2861
lijiaming <outermanjiaming@163.com>
2018-09-06 16:41:16 +0800

    daigou css

--- com.suppresswarnings.html/src/html/js/order.js
+++ com.suppresswarnings.html/src/html/js/order.js
@@ -1,5 +1,40 @@
 
-
+function addorder(order) {
+	var myorders = $("#myorders")
+	var orderinfo = '<div class="order_info">' +
+    	
+    	'订单号：<span>' + order.orderid + '</span><br/>'+
+    	'收货人：<span>' + order.username + '</span><br/>'+
+    	'时间：<span>' + order.time + '</span><br/>'+
+    	'状态：<span>' + order.state + '</span><button>立即支付</button><br/>'+
+    	'详情：<button class="toggleordergoods">展开</button<span>' + order.detail + '</span><br/>'+
+    	'</div>'
+    var ordergoods = '<div class="order_goods" style="display: none;">' + 
+    	'<ul>'
+	var carts = order.carts
+	var size = carts.length
+	for(var k=0;k<size;k++) {
+		var cart = carts[k]
+		var goods = cart.goods
+		var goodsli = '<li>' +
+				        '<div class="inner">' +
+				              '<div class="item_img">' +
+				                '<a href="/detail.html?code='+ticket+'&state='+state+'&goodsid=' +goods.goodsid+ '">' +
+				                   '<img src="'+goods.image+'" title="' +goods.title+ '">'+
+				                '</a>' +
+				              '</div>' +
+				              '<a class="smalla" href="/detail.html?code='+ticket+'&state='+state+'&goodsid=' +goods.goodsid+ '">' + goods.title+ '</a>' +
+				              '<div class="price">' +
+				                '<span>' + goods.price+ '</span>' +
+				                '<em>'+ cart.count + '件</em>' +
+				              '</div>' +
+				            '</div>' +
+				        '</li>'
+		ordergoods = ordergoods + goodsli
+	}
+	ordergoods = ordergoods + '</ul></div>'
+	$('<div>' + orderinfo + ordergoods + '</div>').appendTo(myorders)
+}
 
 
 jQuery.ajax({
@@ -16,16 +51,16 @@ jQuery.ajax({
         console.log('fail to access_token: ' + result)
         index()
       } else {
-        var goodslist = JSON.parse(result)
-        var length = goodslist.length
+        var orderlist = JSON.parse(result)
+        var length = orderlist.length
         for (var k = 0; k < length; k++) {
-        	var goods = goodslist[k]
-        	addone(goods)
+        	var order = orderlist[k]
+        	addorder(order)
         }
       }
     },
     error: function( xhr, result, obj ) {
-      console.log("[lijiaming] collect err: " + result)
+      console.log("[lijiaming] order list err: " + result)
       index()
     }
 })
