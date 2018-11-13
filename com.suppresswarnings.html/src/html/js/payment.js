fa81ccba27150a9260df8a703fbff63fba3e74dc
lijiaming <outermanjiaming@163.com>
2018-11-13 20:19:05 +1300

    index

---
+++ com.suppresswarnings.html/src/html/js/payment.js
@@ -0,0 +1,22 @@
+$.ajax({
+	url : "/wx.http?r=" + Math.random(),
+	data : {
+		action : "payment",
+		random : randnum,
+		ticket : ticket,
+		state : state
+	},
+	success : function(result) {
+		if ("fail" == result) {
+			console.log('fail to access_token: ' + result)
+		} else {
+			console.log('great lijiaming')
+			var goods = JSON.parse(result)
+			$("#reason").text(goods.reason)
+			$("#what").text(goods.what)
+		}
+	},
+	error : function(xhr, result, obj) {
+		console.log("[lijiaming] check payment err: " + result)
+	}
+})
