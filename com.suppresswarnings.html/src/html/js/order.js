52267fe453e61cda5b02f3a5ef50f1968857260a
lijiaming <outermanjiaming@163.com>
2018-12-26 15:32:25 +0800

    order time js in html

--- com.suppresswarnings.html/src/html/js/order.js
+++ com.suppresswarnings.html/src/html/js/order.js
@@ -4,7 +4,7 @@ function toggleordergoods(obj) {
 }
 
 function timestampToTime(timestamp) {
-    var date = new Date(timestamp);
+    var date = new Date(Number(timestamp));
     var Y = date.getFullYear() + '-';
     var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
     var D = date.getDate() + ' ';
