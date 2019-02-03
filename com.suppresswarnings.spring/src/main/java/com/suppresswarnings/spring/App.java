0d08ffaf1b48e48e30b7d042dba2504415fd9eec
lijiaming <outermanjiaming@163.com>
2019-02-03 17:19:43 +0800

    spring boot upload download

--- com.suppresswarnings.spring/src/main/java/com/suppresswarnings/spring/App.java
+++ com.suppresswarnings.spring/src/main/java/com/suppresswarnings/spring/App.java
@@ -2,13 +2,18 @@ package com.suppresswarnings.spring;
 
 import java.util.Arrays;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class App 
 {
     public static void main( String[] args )
@@ -17,18 +22,11 @@ public class App
         SpringApplication.run(App.class, args);
     }
     
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's SEE the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
+    @Bean  
+    public MultipartConfigElement multipartConfigElement() {  
+        MultipartConfigFactory factory = new MultipartConfigFactory();  
+        factory.setMaxFileSize("500MB");  
+        factory.setMaxRequestSize("50MB");  
+        return factory.createMultipartConfig();  
+    }  
 }
--- com.suppresswarnings.spring/src/main/java/com/suppresswarnings/spring/QueryController.java
+++ com.suppresswarnings.spring/src/main/java/com/suppresswarnings/spring/QueryController.java
@@ -1,13 +1,96 @@
 package com.suppresswarnings.spring;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
-@RestController
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
 public class QueryController {
 
-	@RequestMapping("/hello")
+	@GetMapping("/html")
     public String index() {
+		System.out.println("/hello -> index.html");
         return "index.html";
+    }
+	/**
+     * 实现文件上传
+     * */
+    @RequestMapping("fileUpload")
+    @ResponseBody 
+    public String fileUpload(@RequestParam("fileName") MultipartFile file){
+        if(file.isEmpty()){
+            return "false";
+        }
+        String fileName = file.getOriginalFilename();
+        int size = (int) file.getSize();
+        System.out.println(fileName + "-->" + size);
+        
+        String path = "/Users/lijiaming/data" ;
+        File dest = new File(path + "/" + fileName);
+        if(!dest.getParentFile().exists()){
+            dest.getParentFile().mkdir();
+        }
+        try {
+            file.transferTo(dest); 
+            System.out.println(dest.getAbsolutePath());
+            return "true == " + dest.exists();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            return "false";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "false";
+        }
+    }
+    
+    
+    
+    @RequestMapping("download")
+    public String downLoad(HttpServletResponse response){
+        String filename="download.tar";
+        String filePath = "/Users/lijiaming/data/" ;
+        File file = new File(filePath + "/" + filename);
+        if(file.exists()){
+            response.setContentType("application/force-download");
+            response.setHeader("Content-Disposition", "attachment;fileName=" + filename);
+            
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            
+            OutputStream os = null;
+            try {
+                os = response.getOutputStream();
+                fis = new FileInputStream(file); 
+                bis = new BufferedInputStream(fis);
+                int i = bis.read(buffer);
+                while(i != -1){
+                    os.write(buffer);
+                    i = bis.read(buffer);
+                }
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("----------file download " + filename);
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 }
