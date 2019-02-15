2d5f94e3908480cbc9da9b6baefc93c28e726a4a
lijiaming <lijiaming@yingzi.com>
2019-02-15 10:42:42 +0800

    zipper crypt

--- com.suppresswarnings.osgi/c.s.c.common/src/main/java/com/suppresswarnings/corpus/common/Zipper.java
+++ com.suppresswarnings.osgi/c.s.c.common/src/main/java/com/suppresswarnings/corpus/common/Zipper.java
@@ -18,8 +18,8 @@ import java.util.zip.Inflater;
  *
  */
 public class Zipper {
-	public static byte[] lijiaming = {11,8,9,8,0,12,8,13,6};
 	public static byte[] compresser(byte[] input) {
+		byte[] lijiaming = array2byte(string2byte(System.getProperty("zipper.password", "changeit")));
 		// Compress the bytes
 		byte[] output = new byte[input.length];
 		Deflater compresser = new Deflater();
@@ -32,28 +32,47 @@ public class Zipper {
 		System.arraycopy(head, 0, store, 0, 4);
 		System.arraycopy(output, 0, store, 4, compressedDataLength);
 		System.arraycopy(lijiaming, 0, store, 4 + compressedDataLength, lijiaming.length);
+		xor(store, lijiaming);
 		return store;
 	}
 
 	public static byte[] decompresser(byte[] output) {
+		byte[] lijiaming = array2byte(string2byte(System.getProperty("zipper.password", "changeit")));
 		// uncompress the bytes
 		int compressedDataLength = byte2int(output);
 		Inflater decompresser = new Inflater();
-		decompresser.setInput(output, 4, output.length - 4 - lijiaming.length);
-		byte[] result = new byte[compressedDataLength];
 		try {
+			xor(output, lijiaming);
+			decompresser.setInput(output, 4, output.length - 4 - lijiaming.length);
+			byte[] result = new byte[compressedDataLength];
 			decompresser.inflate(result);
+			decompresser.end();
+			return result;
 		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}
-		decompresser.end();
-		return result;
+		
 	}
 
 	public static byte[] int2byte(int res) {
 		return new byte[] { (byte) (res & 0xff), (byte) ((res >> 8) & 0xff), (byte) ((res >> 16) & 0xff), (byte) (res >>> 24) };
 	}
-
+	public static byte[] array2byte(int[] array) {
+		byte[] bytes = new byte[array.length];
+		for(int i=0;i<array.length;i++) bytes[i] = (byte)array[i];
+		return bytes;
+	}
+	public static void xor(byte[] origin, byte[] password) {
+		for(int i=0,j=0;i<origin.length;i++,j++) {
+			if(j==password.length) j=0;
+			origin[i] ^= password[j];
+		}
+	}
+	public static int[] string2byte(String password) {
+		return password.chars().map(x->x-'a').toArray();
+	}
+	
 	public static int byte2int(byte[] res) {
 		return (res[0] & 0xff) | ((res[1] << 8) & 0xff00) | ((res[2] << 16) & 0xff0000) | (res[3] << 24);
 	}
