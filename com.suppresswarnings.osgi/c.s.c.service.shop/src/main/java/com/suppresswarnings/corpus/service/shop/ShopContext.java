0643e365300f46e317c7d7efa559c5bd53bc7f79
lijiaming <outermanjiaming@163.com>
2019-02-17 15:53:22 +0800

    shop & reply

--- com.suppresswarnings.osgi/c.s.c.service.shop/src/main/java/com/suppresswarnings/corpus/service/shop/ShopContext.java
+++ com.suppresswarnings.osgi/c.s.c.service.shop/src/main/java/com/suppresswarnings/corpus/service/shop/ShopContext.java
@@ -22,12 +22,12 @@ import com.suppresswarnings.corpus.common.State;
 import com.suppresswarnings.corpus.service.CorpusService;
 import com.suppresswarnings.corpus.service.WXContext;
 import com.suppresswarnings.corpus.service.work.Quiz;
+import com.suppresswarnings.corpus.service.wx.WXuser;
 
 public class ShopContext extends WXContext {
 	public static final String CMD = "我的商铺";
 	public static final String Wait = "Wait";
 	public static final String None = "None";
-	String qrcode = null;
 	List<KeyValue> quiz = new ArrayList<>();
 	State<Context<CorpusService>> shop = new State<Context<CorpusService>>() {
 		boolean finish = false;
@@ -38,9 +38,10 @@ public class ShopContext extends WXContext {
 
 		@Override
 		public void accept(String t, Context<CorpusService> u) {
-			u.output("管理你的商铺，你可以输入一下命令：");
+			u.output("您可以输入以下命令：");
 			//check binded?
 			u.output("    " + bind.name());
+			//check eligible
 			u.output("    " + ad.name());
 		}
 
@@ -80,7 +81,7 @@ public class ShopContext extends WXContext {
 			String keyBindTime = String.join(Const.delimiter, Const.Version.V1, openid(), "Shop", "BindTime");
 			u.content().account().put(keyBind, Wait);
 			u.content().account().put(keyBindTime, time());
-			u.output("请扫描商铺二维码（请联系[素朴网联]工作人员索取商铺二维码）");
+			u.output("请扫描商铺二维码（请联系【素朴网联】工作人员申请商铺二维码）");
 			finish = true;
 		}
 
@@ -103,8 +104,9 @@ public class ShopContext extends WXContext {
 		
 		
 	};
-	
+	List<String> openids = new ArrayList<>();
 	State<Context<CorpusService>> ad = new State<Context<CorpusService>>() {
+		long limit = 100;
 		boolean finish = false;
 		
 		/**
@@ -114,9 +116,32 @@ public class ShopContext extends WXContext {
 
 		@Override
 		public void accept(String t, Context<CorpusService> u) {
+			finish = true;
 			//1.check if I am Boss
+			String keyBind = String.join(Const.delimiter, Const.Version.V1, openid(), "Shop", "Bind");
+			String binded = u.content().account().get(keyBind);
+			if(binded == null || None.equals(binded)) {
+				u.output("您还没有绑定商铺二维码，发广告给谁呀？");
+			} else {
+				String qrScene = binded;
+				String head = String.join(Const.delimiter, Const.Version.V1, openid(), "Shop", "Customer");
+				List<String> userids = new ArrayList<>();
+				u.content().account().page(head, head, null, limit, (k, v) ->{
+					userids.add(v);
+				});
+				logger.info("[Shop ad] qrScene: " + qrScene);
+				for(String userid : userids) {
+					WXuser user = u.content().getWXuserByOpenId(userid);
+					if(user != null && user.getSubscribe() == 1) {
+						openids.add(userid);
+					} else {
+						logger.error("[Shop ad] user gone: " + user);
+					}
+				}
+			}
 			//2.Y check binded, N go to 10 quiz
-			u.output("发广告" + t);
+			logger.info("[Shop ad] has user size: " + openids.size());
+			u.output("请输入要发送的广告内容（请仔细阅读好再发送）：");
 		}
 
 		@Override
@@ -126,7 +151,7 @@ public class ShopContext extends WXContext {
 			}
 			//get out
 			finish = false;
-			return init;
+			return send;
 		}
 
 		@Override
@@ -140,6 +165,59 @@ public class ShopContext extends WXContext {
 		}
 		
 	};
+	State<Context<CorpusService>> send = new State<Context<CorpusService>>() {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -3735870889232510969L;
+
+		@Override
+		public void accept(String t, Context<CorpusService> u) {
+			String keyAdquota = String.join(Const.delimiter, Const.Version.V1, openid(), "Shop", "Ad", "Quota");
+			String keyLimit = String.join(Const.delimiter, Const.Version.V1, openid(), "Shop", "Ad", "Limit");
+			String quota = u.content().account().get(keyAdquota);
+			String limit = u.content().account().get(keyLimit);
+			if(limit == null) {
+				limit = "10";
+				u.content().account().put(keyLimit, limit);
+			}
+			if(quota == null) {
+				quota = limit;
+				u.content().account().put(keyAdquota, quota);
+			}
+
+			Integer value = Integer.valueOf(quota);
+			value = value - 1;
+			quota = "" + value;
+			u.content().account().put(keyAdquota, quota);
+			
+			if(value >= 0) {
+				String message = t;
+				openids.stream().distinct().forEach(userid ->u.content().atUser(userid, message));
+				u.output("您的广告已经飞奔到顾客手中！\n珍惜广告额度，请勿重复发送！\n剩余额度：" + quota);
+			} else {
+				u.output("您到广告额度已经不足，请联系【素朴网联】工作人员恢复！");
+			}
+		}
+
+		@Override
+		public State<Context<CorpusService>> apply(String t, Context<CorpusService> u) {
+			return shop;
+		}
+
+		@Override
+		public String name() {
+			return null;
+		}
+
+		@Override
+		public boolean finish() {
+			return false;
+		}
+		
+	};
+	
 	
 	State<Context<CorpusService>> scan = new State<Context<CorpusService>>() {
 		boolean finish = false;
@@ -153,7 +231,6 @@ public class ShopContext extends WXContext {
 		public void accept(String t, Context<CorpusService> u) {
 			finish = true;
 			String qrScene = t.substring("SCAN_".length());
-			qrcode = qrScene;
 			//1.check if I am Boss
 			String keyBind = String.join(Const.delimiter, Const.Version.V1, openid(), "Shop", "Bind");
 			String binded = u.content().account().get(keyBind);
@@ -168,8 +245,12 @@ public class ShopContext extends WXContext {
 				if(ownerid == null) {
 					u.output("该商铺二维码还未绑定！");
 				} else {
-					String keyCustomer = String.join(Const.delimiter, Const.Version.V1, ownerid, "Shop", "Customer", time(), random());
+					String keyCustomer = String.join(Const.delimiter, Const.Version.V1, ownerid, "Shop", "Customer", openid());
 					u.content().account().put(keyCustomer, openid());
+					String keyHistoryCustomer = String.join(Const.delimiter, Const.Version.V1, ownerid, "Shop", "History", "Customer", time(), random());
+					u.content().account().put(keyHistoryCustomer, openid());
+					String keyUser = String.join(Const.delimiter, Const.Version.V1, "Shop", qrScene, "Customer", time(), random());
+					u.content().account().put(keyUser, openid());
 					isCustomer = true;
 					u.output("请留下您的手机号，中奖后需要凭手机号领奖！");
 				}
@@ -254,12 +335,9 @@ public class ShopContext extends WXContext {
 			
 			i ++;
 			if(first) {
-				String keyPhone = String.join(Const.delimiter, Const.Version.V1, openid(), "Contact", "Phone");
-				String phone = u.content().account().get(keyPhone);
-				if(phone != null) {
-					String keyHistory = String.join(Const.delimiter, Const.Version.V1, openid(), "Contact", "PhoneHistory", time());
-					u.content().account().put(keyHistory, phone);
-				}
+				String keyPhone = String.join(Const.delimiter, Const.Version.V1, openid(), "Shop", "Contact", "Phone");
+				String keyHistory = String.join(Const.delimiter, Const.Version.V1, openid(), "Shop", "Contact", "History", "Phone", time());
+				u.content().account().put(keyHistory, t);
 				u.content().account().put(keyPhone, t);
 				logger.info("[Shop customer] save phone: " + openid() + " => " + t);
 				u.output("请回答10个问题，大奖等你来拿！");
