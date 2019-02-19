1ea7e653f3bda3208578d0e6527cd4973e24fca9
lijiaming <lijiaming@yingzi.com>
2019-02-19 10:49:24 +0800

    total

--- com.suppresswarnings.html/src/html/js/payment.js
+++ com.suppresswarnings.html/src/html/js/payment.js
@@ -20,6 +20,7 @@ $.ajax({
 			$("#userimg").attr("src", goods.userimg)
 			if(goods.type == "Auth") {
 				input.hide()
+				var rate = 1
 				var total = goods.pricecent
 				var price = 0.01 * parseFloat(goods.pricecent);
 				price = price.toFixed(2)
@@ -29,6 +30,7 @@ $.ajax({
 				input.show()
 				input.val("");
 				input.focus()
+				rate = goods.pricecent
 				new KeyBoard(document.getElementById('amount'), 2);
 				changeval(input)
 			}
