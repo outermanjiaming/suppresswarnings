e89e85278937d4c0347c41f00da1c0f3f7cd848a
lijiaming <outermanjiaming@163.com>
2019-04-21 15:30:10 +0800

    game

--- com.suppresswarnings.osgi/c.s.c.service.likeshare/src/main/java/com/suppresswarnings/corpus/service/likeshare/LikeShareContext.java
+++ com.suppresswarnings.osgi/c.s.c.service.likeshare/src/main/java/com/suppresswarnings/corpus/service/likeshare/LikeShareContext.java
@@ -1,6 +1,9 @@
 package com.suppresswarnings.corpus.service.likeshare;
 
+import java.util.Random;
+
 import com.google.gson.Gson;
+import com.suppresswarnings.corpus.common.Const;
 import com.suppresswarnings.corpus.common.Context;
 import com.suppresswarnings.corpus.common.State;
 import com.suppresswarnings.corpus.service.CorpusService;
@@ -38,7 +41,11 @@ public class LikeShareContext extends WXContext {
 			if(CMD.equals(t)) {
 				return likeshare;
 			}
+			logger.info("like share input: " + t);
 			if(t.startsWith("SCAN_")) {
+				if(t.startsWith("SCAN_P_Like_Game")) {
+					return likegame;
+				}
 				return likeshare;
 			}
 			return init;
@@ -53,6 +60,58 @@ public class LikeShareContext extends WXContext {
 		public boolean finish() {
 			return false;
 		}
+	};
+	
+	State<Context<CorpusService>> likegame = new State<Context<CorpusService>>() {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 6040006707683444576L;
+
+		@Override
+		public void accept(String t, Context<CorpusService> u) {
+			synchronized (likegame) {
+				String done = String.join(Const.delimiter, Const.Version.V2, "Like", "Game", openid());
+				String exist = u.content().account().get(done);
+				if(exist == null) {
+					u.content().account().put(done, time());
+					u.content().data().put(String.join(Const.delimiter, Const.Version.V2, "Like", "Game", openid(), time()), openid());
+					u.content().data().put(String.join(Const.delimiter, Const.Version.V2, openid(), "Like", "Game", time()), openid());
+					
+					double score = new Random().nextDouble();
+					if(score < 0.6) {
+						u.content().account().put(String.join(Const.delimiter, Const.Version.V2, openid(), "Project", "Like", "Money", time()), "3");
+						u.output("恭喜你，抽中三等奖，可以直接输入「我要提现」申请提现哦，秒到账！");
+					} else if(score > 0.9) {
+						u.content().account().put(String.join(Const.delimiter, Const.Version.V2, openid(), "Project", "Like", "Money", time()), "100");
+						u.output("恭喜你，抽中一等奖，可以直接输入「我要提现」申请提现哦，秒到账！");
+					} else {
+						u.content().account().put(String.join(Const.delimiter, Const.Version.V2, openid(), "Project", "Like", "Money", time()), "10");
+						u.output("恭喜你，抽中二等奖，可以直接输入「我要提现」申请提现哦，秒到账！");
+					}
+				} else {
+					u.content().account().put(String.join(Const.delimiter, Const.Version.V2, "Inform", "Like", "Game", time(), random()), openid());
+					u.output("你已经参与抽奖活动了，奖金已经在你的账户，你可以输入「我要提现」申请提现哦，秒到账！");
+				}
+			}
+		}
+
+		@Override
+		public State<Context<CorpusService>> apply(String t, Context<CorpusService> u) {
+			return init;
+		}
+
+		@Override
+		public String name() {
+			return "点赞抽奖活动";
+		}
+
+		@Override
+		public boolean finish() {
+			return true;
+		}
+		
 	};
 	
 	public LikeShareContext(String wxid, String openid, CorpusService ctx) {
