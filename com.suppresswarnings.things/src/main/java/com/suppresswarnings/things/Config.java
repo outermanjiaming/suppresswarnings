635548d41a3f936193b1c61696de115cbc8395ca
lijiaming <outermanjiaming@163.com>
2019-04-23 15:29:13 +0800

    split file

--- com.suppresswarnings.things/src/main/java/com/suppresswarnings/things/Config.java
+++ com.suppresswarnings.things/src/main/java/com/suppresswarnings/things/Config.java
@@ -5,6 +5,8 @@ import javax.net.SocketFactory;
 import com.suppresswarnings.things.security.a;
 import com.suppresswarnings.things.security.b;
 import com.suppresswarnings.things.security.c;
+import com.suppresswarnings.things.security.d;
+import com.suppresswarnings.things.security.e;
 
 @SuppressWarnings("用于创建和素朴网联的Socket连接")
 public interface Config{
@@ -12,11 +14,11 @@ public interface Config{
 	static byte[] code() {
 		byte[][] bbs = {
 				a.b1,a.b2,a.b3,a.b4,a.b5,a.b6,a.b7,a.b8,
-				a.b9,a.b10,
+				d.b9,d.b10,
 				b.b11,b.b12,b.b13,b.b14,b.b15,b.b16,b.b17,
 				b.b18,c.b19,c.b20,
 				c.b21,c.b22,c.b23,c.b24,c.b25,c.b26,c.b27,
-				b.b28,b.b29
+				e.b28,e.b29
 		};
 		int size = 0;
 		for(byte[] bs : bbs) {
