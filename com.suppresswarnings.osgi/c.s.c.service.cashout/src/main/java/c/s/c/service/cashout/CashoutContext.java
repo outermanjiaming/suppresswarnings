98235c2ca66bb608cb609e220d57e2ffbd50c4c7
lijiaming <outermanjiaming@163.com>
2019-04-25 15:23:40 +0800

    payment close

--- com.suppresswarnings.osgi/c.s.c.service.cashout/src/main/java/c/s/c/service/cashout/CashoutContext.java
+++ com.suppresswarnings.osgi/c.s.c.service.cashout/src/main/java/c/s/c/service/cashout/CashoutContext.java
@@ -12,6 +12,8 @@ import com.suppresswarnings.corpus.service.WXContext;
 public class CashoutContext extends WXContext {
 	public static final String CMD = "我要提现";
 	public static final String[] AUTH = {"Cashout"};
+	String request;
+	String realValue;
 	State<Context<CorpusService>> cashout = new State<Context<CorpusService>>() {
 
 		/**
@@ -28,23 +30,26 @@ public class CashoutContext extends WXContext {
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHH");
 				String hourly = sdf.format(new Date());
 				u.content().account().put(key, time());
-				u.content().account().put(String.join(Const.delimiter, Const.Version.V2, "Cashout", "Request", hourly, time(), openid()), openid());
+				request = String.join(Const.delimiter, Const.Version.V2, "Cashout", "Request", hourly, time(), openid());
+				u.content().account().put(request, openid());
 				u.content().account().put(String.join(Const.delimiter, Const.Version.V2, openid(), "Cashout", "Request", hourly), time());
 				u.output("恭喜你，提现请求已经发出，财务部门正在审核，预计24小时内审核到账");
 			} else {
 				//is doing
 				u.output("你的提现请求正在审核，预计24小时内审核到账");
 			}
+			u.output("如果要补充实名信息请输入：" + real.name());
 			
 		}
 
 		@Override
 		public State<Context<CorpusService>> apply(String t, Context<CorpusService> u) {
-			if(CMD.equals(t)) {
+			if(CMD.equals(t) || t.startsWith("SCAN_")) {
 				return cashout;
 			}
-			if(t.startsWith("SCAN_")) {
-				return cashout;
+			
+			if(realValue == null || real.name().equals(t)) {
+				return real;
 			}
 			return init;
 		}
@@ -59,10 +64,44 @@ public class CashoutContext extends WXContext {
 			return false;
 		}
 	};
+	
+	
+	State<Context<CorpusService>> real = new State<Context<CorpusService>>() {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 7051846342563147949L;
+
+		@Override
+		public void accept(String t, Context<CorpusService> u) {
+			u.output("你还没有实名认证，提现需要实名信息（微信要求），请输入该微信号对应的真实姓名和身份证，比如：张三,430481199801107112");
+		}
+
+		@Override
+		public State<Context<CorpusService>> apply(String t, Context<CorpusService> u) {
+			realValue = t;
+			u.content().account().put(String.join(Const.delimiter, Const.Version.V2, openid(), "RealValue", time()), t);
+			u.content().account().put(String.join(Const.delimiter, Const.Version.V2, openid(), "RealValue"), realValue);
+			return cashout;
+		}
+
+		@Override
+		public String name() {
+			return "实名认证";
+		}
+
+		@Override
+		public boolean finish() {
+			return false;
+		}
+		
+	};
 
 	public CashoutContext(String wxid, String openid, CorpusService ctx) {
 		super(wxid, openid, ctx);
 		this.state = cashout;
+		this.realValue = ctx.account().get(String.join(Const.delimiter, Const.Version.V2, openid(), "RealValue"));
 	}
 
 }
