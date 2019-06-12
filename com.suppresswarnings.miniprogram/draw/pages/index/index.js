404454dbdc157529d5ff6d4c8665143a5c66447d
lijiaming <outermanjiaming@163.com>
2019-06-12 14:20:19 +0800

    canvas lines

--- com.suppresswarnings.miniprogram/draw/pages/index/index.js
+++ com.suppresswarnings.miniprogram/draw/pages/index/index.js
@@ -37,6 +37,10 @@ Page({
     that.sys();
     console.log("hello world");
     const ctx = wx.createCanvasContext('canvas');
+
+    this.lines(ctx, 320, 320, 20, 20);
+    ctx.stroke();
+
     ctx.setFillStyle('red');
     ctx.fillRect(20, 120, 50, 50);
     
@@ -49,7 +53,43 @@ Page({
     ctx.setFillStyle('yellow');
     ctx.fillRect(20, 180, 250, 50);
 
+    ctx.setFillStyle('pink');
+    ctx.fillRect(280, 120, 20, 110);
+
     ctx.draw();
+
+  },
+  lines: function(ctx, xmax, ymax,dx, dy) {
+    var startx = 0;
+    var starty = 0;
+    while(true) {
+      startx = 0;
+      starty += dy;
+      if (starty > ymax) {
+        break;
+      }
+      ctx.setStrokeStyle("black");
+      ctx.setLineWidth(1);
+      ctx.moveTo(startx, starty);
+      ctx.lineTo(xmax, starty);
+    }
+
+    while (true) {
+      starty = 0;
+      startx += dx;
+      if (startx > xmax) {
+        break;
+      }
+      ctx.setStrokeStyle("black");
+      ctx.setLineWidth(1);
+      ctx.moveTo(startx, starty);
+      ctx.lineTo(startx, ymax);
+    }
+    startx += dx;
+    if (startx > xmax) {
+
+
+    }
   },
   sys: function () {
     var that = this;
